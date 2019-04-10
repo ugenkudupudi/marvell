276126e542a3127a3aadf89ced90e600b8ee716b
ugen <ugen@kudupudi.com>
2019-04-10 16:46:51 +0530

    testpmd file

---
+++ qemu/root/testpmd.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+/usr/local/bin/testpmd -- \
+--burst=256 --txd=2048 --rxd=1024 --rxq=1 --txq=1 --nb-cores=1 \
+--coremask 2 -a --forward-mode=io
+if [[ $? -ne 0 ]] ; then
+        exit $?
+fi
